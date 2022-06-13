2b423c611f5b133e000b0d2cc57f5eb5831cfbc5
MMOUZOLIG3\mouzo <mouzosilvam@gmail.com>
2022-06-13 10:03:33 +0200

    add header

--- spot-film/src/App.js
+++ spot-film/src/App.js
@@ -1,24 +1,20 @@
-import 'swiper/css';
-import './assets/boxicons-2.1.2/css/boxicons.min.css';
-import './App.scss';
+import "swiper/css";
+import "./assets/boxicons-2.1.2/css/boxicons.min.css";
+import "./App.scss";
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
-import Routes from './config/Routes';
+import Routes from "./config/Routes"
 
 function App() {
   return (
     <BrowserRouter>
-      <Route render={props => (
-        <>
-          <Header {...props} />
-          <Routes />
-          <Footer />
-        </>
-      )} />
+      <Header />
+      <Routes />
+      <Footer />
     </BrowserRouter>
   );
 }
