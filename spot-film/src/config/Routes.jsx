5defb0d600f28a99cf4820de8b92d16a59d2bc5d
MMOUZOLIG3\mouzo <mouzosilvam@gmail.com>
2022-08-05 11:16:10 +0200

    datil movies half-done and some refactorings

--- spot-film/src/config/Routes.jsx
+++ spot-film/src/config/Routes.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import Home from "../pages/Home";
 import Catalog from "../pages/Catalog";
-import Detail from "../pages/Detail";
+import Detail from "../pages/datail/Detail";
+import Home from "../pages/Home";
 
 export default () => {
   return (
     <Routes>
-      <Route path="/:category/search/:keyword" element={<Catalog/>} />
-      <Route path="/:category/:id" element={<Detail/>} />
-      <Route path="/:category" element={<Catalog/>} />
-      <Route path="/" exact element={<Home/>} />
+      <Route path="/:category/search/:keyword" element={<Catalog />} />
+      <Route path="/:category/:id" element={<Detail />} />
+      <Route path="/:category" element={<Catalog />} />
+      <Route path="/" exact element={<Home />} />
     </Routes>
   );
 };
